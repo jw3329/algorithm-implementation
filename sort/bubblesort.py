59059b2818410a02d3275a43abe08ddcee8d5a45
Junwon Jung <junwon3329@gmail.com>
2020-01-28 21:09:03 -0600

    Committing another bubblesort method, sorting by backward first

--- sort/bubblesort.py
+++ sort/bubblesort.py
@@ -16,10 +16,19 @@ def bubblesort(arr):
             break
 
 
+# sorting by backward,
+def bubblesort2(arr):
+    for i in range(len(arr)):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                swap(arr, j, j + 1)
+
+
 import random
 
 arr = random.sample(range(1000), 100)
-print('before sorted value:', arr)
+print('before sorted value:\n', arr)
 bubblesort(arr)
-print('after calling function:', arr)
+print('after calling bubblesort:\n', arr)
+print('after calling bubblesort2:\n', arr)
 print('sorted?', sorted(arr) == arr)
